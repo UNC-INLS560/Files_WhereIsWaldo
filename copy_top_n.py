58197fce1713640df80840322f706a0ff98b4d3d
David Gotz <gotz@unc.edu>
2026-09-15 22:01:20 -0400

    Add exercise requirements to copy_top_n.py

---
+++ copy_top_n.py
@@ -0,0 +1,5 @@
+# Write a program that lets a user copy the census-first-names.txt data file to
+# a new file, but only keep the “Top N” names.
+#
+# The user should choose how many names to include in the copy, and what the new
+# file's name should be.
