8a0955d8b6296e3ac031e21a0a649f35303163f3
David Gotz <gotz@unc.edu>
2026-09-15 21:59:19 -0400

    Add exercise requirements to wheres_name.py

---
+++ wheres_name.py
@@ -0,0 +1,3 @@
+# Modify your "Where's Waldo" program to instead ask the user for a name. Then search
+# for that name in the census-first-names.txt data file. If the name is found, show the
+# rank. If the name is not found, tell the user with a meaningful message.
