4fe9295b8f59b34ba065aab753640631894bcfe6
David Gotz <gotz@unc.edu>
2026-09-15 21:56:59 -0400

    Add exercise requirements to wheres_waldo.py
    Described the exercise in the comments.

---
+++ wheres_waldo.py
@@ -0,0 +1,3 @@
+# The census-first-names.txt data file contains names in ranked order by popularity 
+# according to US Census data. Write a program that finds the name Waldo in the
+# data file and tells you the name's rank according to the file.
